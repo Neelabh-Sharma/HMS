d93efc9d98378a8b99d2fa1a04cd207d4d95a033
Neelabh-Sharma <neelabhsharma2002@gmail.com>
2025-01-26 15:09:14 -0800

    update V-1.0

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,11 @@ app.use(cors());
 app.use(express.json());
 // app.use(logger);
 
+// default route
+app.get('/',(req,res) =>{
+    return res.send("API is working")
+})
+
 // Routes
 app.use('/api/users', userRoutes);
 
